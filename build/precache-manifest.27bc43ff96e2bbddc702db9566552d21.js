ce0155520bbd03ad38e0b4bd079688c0e6803c88
James-E-Jackson <jedward76@gmail.com>
2021-04-05 09:46:52 -0400

    readme update

---
+++ build/precache-manifest.27bc43ff96e2bbddc702db9566552d21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1785aceaca1a1b2a3d7e9e819ca596",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b930d4bfe6664da4b64e",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "223b709c9b9c0b308480",
+    "url": "/static/js/2.dcb25545.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.dcb25545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b930d4bfe6664da4b64e",
+    "url": "/static/js/main.0915aa4b.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
